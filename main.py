2c17cdba3251537ebeb9b632ca4a80e71fd00aef
CrispyCabot <18431358+CrispyCabot@users.noreply.github.com>
2019-03-11 13:49:05 -0400

    sdaf

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import pygame
+import os
 
-#bla bla
+pygame.init()
+
+width = 1920
+height = 1080
+
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Game")
+#pygame.display.set_icon('')
+PATH = os.path.abspath(__file__) #This gets the whole path so like: /Users/user/folder/Tanks/Tanks.py
+PATH = PATH[0:-7] #-7 to chop off main.py
